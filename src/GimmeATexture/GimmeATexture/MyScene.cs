80340ccf97ff29d4d7acb41c917bf9fa817d2018
Matthew Erbs <matthew.erbs@merbla.com>
2014-12-06 08:42:09 +1000

    Added example

---
+++ src/GimmeATexture/GimmeATexture/MyScene.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+using CoreGraphics;
+using Foundation;
+using SpriteKit;
+using UIKit;
+using System.Linq;
+
+namespace GimmeATexture
+{
+	public class MyScene : SKScene
+	{
+		SKTextureAtlas telephones;
+
+		public MyScene (CGSize size) : base (size)
+		{
+			BackgroundColor = new UIColor (0.15f, 0.15f, 0.3f, 1.0f);
+			telephones = SKTextureAtlas.FromName ("Telephone");
+		}
+
+		public override void TouchesBegan (NSSet touches, UIEvent evt)
+		{ 
+			foreach (var touch in touches) {
+
+				var touchLocation = ((UITouch)touch).LocationInNode (this);
+				var telephoneSprite = new SKSpriteNode ("Telephone_1");
+				telephoneSprite.Position = touchLocation;
+				 
+				var telephoneList = telephones.TextureNames
+					.Select (x => telephones.TextureNamed (x))
+					.ToArray();
+				 
+				var animationAction = SKAction.AnimateWithTextures (telephoneList, 0.05);
+
+				telephoneSprite.RunAction(SKAction.RepeatActionForever(animationAction));
+
+				AddChild (telephoneSprite);
+				  
+			}
+		}
+
+		public override void Update (double currentTime)
+		{
+			// Run before each frame is rendered
+			base.Update (currentTime);
+		}
+	}
+}
---
+++ src/GimmeATexture/GimmeATexture/ViewController.designer.cs
@@ -0,0 +1,13 @@
+﻿using Foundation;
+using System.CodeDom.Compiler;
+
+namespace GimmeATexture
+{
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		void ReleaseDesignerOutlets ()
+		{
+		}
+	}
+}
